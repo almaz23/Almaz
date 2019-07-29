c8db97b5b1397e175b705ae0a58554e45a335d7f
almaz23 <52189775+almaz23@users.noreply.github.com>
2019-07-29 11:27:05 +0530

    Create pos.neg.zero.c

---
+++ pos.neg.zero.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("positive");
+	}
+	else if(n<0)
+	{
+		printf("negative");
+	}
+	else
+	{
+		printf("zero");
+	}
+	return 0;
+}
