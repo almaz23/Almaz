88ddc31db295eee00721e80ea8a43a9eb4c58003
almaz23 <52189775+almaz23@users.noreply.github.com>
2019-07-30 20:59:06 +0530

    Create alphabetnot.c

---
+++ alphabetnot.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	char ch;
+	scanf("%c",&ch);
+	if(ch>=65&&ch<=90 || ch>=97&&ch<=122)
+	{
+		printf("Alphabet");
+	}
+	else
+	{
+		printf("No");
+	}
+	return 0;
+}
